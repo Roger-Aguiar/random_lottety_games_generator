241f12eab3682ea68be2f4a97b2b49e9961692ef
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2021-06-04 18:08:47 -0300

    It updates the GenerateGames function

--- Program.cs
+++ Program.cs
@@ -6,12 +6,16 @@ namespace LotteryGameGenerator
     class Program
     {
         static void Main(string[] args)
-        {
+        {       
+            Console.Title = "Lottery Game Generator";
+                 
             GenerateGames();
         }
 
         static void GenerateGames()
         {
+            Console.WriteLine("Welcome to the Lottery Game Generator! Enter the following data for generating your games!\n\n");
+
             Console.Write("Enter the number of games that you want to generate: ");
             var quantityOfGames = Int32.Parse(Console.ReadLine());
 
@@ -25,14 +29,14 @@ namespace LotteryGameGenerator
             var lastNumber = Int32.Parse(Console.ReadLine());
 
             Console.WriteLine("\nYour games. Good luck!\n");
-            
+
             for(var quantity = 0; quantity < quantityOfGames; quantity++)
             {
                 IGameGenerator gameGenerator = new GameGenerator(quantityOfNumbers, firstNumber, lastNumber);
                 List<int> game = gameGenerator.GenerateGame();
 
                 IGameLayout gameLayout = new GameLayout(game);
-                Console.WriteLine(gameLayout.GetLayoutGame());
+                Console.WriteLine("Game " + (quantity + 1).ToString("00") + ":  " + gameLayout.GetLayoutGame());
             }
         }
     }
